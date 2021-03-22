c270c121b9c2752f461e5f09546c7abf5ec4b8d7
Jahkwan Taillefer <jahkwan10@gmail.com>
2021-03-22 22:27:40 +0000

    solved the product function.

--- main.fs
+++ main.fs
@@ -4,16 +4,17 @@ type Tree =
     | Node of Tree * Tree
     | Leaf of int
 
-
-//return the leaf's value to the previous node; use the value within the node to 
 let rec prod (t:Tree) :int =
     match t with
     | Leaf x -> x
-    | Node (l,r) -> prod(t)// should first look down a node's left side then it's right, but how would it determine if a side's been checked already?
-    | _ -> 1 //if there's no nodes or something just give it a 1.
+    | Node (l,r) -> prod(l) * prod(r)
+
+    //OO DID IT.
 
 let rec map (f:int->int) (t:Tree) :Tree =
-    t
+    match t with
+    //| Leaf x -> (f x)
+    | Node(l,r) -> map f l
 
 let rec foldStr (nf:string -> string -> string) (lf:int->string) (t:Tree) :string =
     ""
