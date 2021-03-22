f239805c2aed1ec00196332175aaff01e09e3368
Jahkwan Taillefer <jahkwan10@gmail.com>
2021-03-22 21:55:59 +0000

    .

--- main.fs
+++ main.fs
@@ -4,8 +4,13 @@ type Tree =
     | Node of Tree * Tree
     | Leaf of int
 
+
+//return the leaf's value to the previous node; use the value within the node to 
 let rec prod (t:Tree) :int =
-    0
+    match t with
+    | Leaf x -> x
+    | Node (l,r) -> prod(t)// should first look down a node's left side then it's right, but how would it determine if a side's been checked already?
+    | _ -> 1 //if there's no nodes or something just give it a 1.
 
 let rec map (f:int->int) (t:Tree) :Tree =
     t
